90d9b40e6f64d4d628b374c48e46161bb9178300
AzwarLaOde <azwaralfayyadh@gmail.com>
2023-05-30 15:37:53 +0800

    commit

---
+++ app/src/main/java/com/example/adajob/ui/auth/register/RegisterActivity.kt
@@ -0,0 +1,14 @@
+package com.example.adajob.ui.auth.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.adajob.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        supportActionBar?.hide()
+    }
+}
